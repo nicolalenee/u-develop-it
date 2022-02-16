fe8296cca8f49ca07bf1a64f891a4965bcb76737
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-02-15 23:49:52 -0500

    build the database calls

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to the database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // SQL username
+    user: 'root',
+    // SQL password
+    password: '${SunFlower29}',
+    database: 'election'
+  },
+  console.log("Connected to the election database.")
+);
 
 // Test Express.js connection
 // app.get('/', (req, res) => {
@@ -11,6 +25,10 @@ app.use(express.json());
 //     message: 'Hello World'
 //   })
 // })
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request(Not Found)
 app.use((req, res) => {
