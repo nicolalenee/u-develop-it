c8e31d965c684fe0f5063616d74f9de2fb8edfc8
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-02-24 18:46:49 -0500

    add left join query

--- server.js
+++ server.js
@@ -71,7 +71,12 @@ app.post('/api/candidate', ({ body }, res) => {
 
 // Create a candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates,*, parties.name
+              AS party_name
+              FROM candidates
+              LEFT JOIN parties
+              ON candidates.party_id = parties.id
+              WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -88,7 +93,11 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name
+              AS party_name
+              FROM candidates
+              LEFT JOIN parties
+              ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
