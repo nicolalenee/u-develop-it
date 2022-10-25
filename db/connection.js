7a19b3087c10bb11b8c092ea7edbbde437587d39
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-10-24 21:07:43 -0400

    update env vars

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // connect to the database
 const db = mysql.createConnection(
   {
     host: 'localhost',
     // SQL username
-    user: 'root',
+    user: process.env.DB_USER,
     // SQL password
-    password: '${SunFlower29}',
-    database: 'election'
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
   },
   console.log("Connected to the election database.")
 );
